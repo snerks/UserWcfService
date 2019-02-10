5e17f61fa8a4436c5dac1fd5a1f68e5868910171
snerks <martin.github@outlook.com>
2019-02-10 10:32:40 +0000

    Add notes on endpointConfigurationName

--- UserWebApp/Controllers/HomeController.cs
+++ UserWebApp/Controllers/HomeController.cs
@@ -33,6 +33,20 @@ namespace UserWebApp.Controllers
             //    new EndpointAddress("http://localhost:58314/UserService.svc"));
 
             //Channel = new ChannelFactory<T>("BasicHttpBinding_IUserService");
+
+            // The optional name attribute uniquely identifies an endpoint for a given contract. 
+            // It is used by the ChannelFactory<TChannel> or by the ClientBase<TChannel> to specify 
+            // which endpoint in the client configuration is being targeted and must be loaded when 
+            // a channel is created to service. 
+            //
+            // A wildcard endpoint configuration name "*" is available and indicates to the 
+            // ApplyConfiguration method that it should load any endpoint configuration in the file, 
+            // provided there is precisely one available, and otherwise throws an exception.
+            //
+            // If this attribute is omitted, the corresponding endpoint is used as the default endpoint 
+            // associated with the specified contract type. 
+            // The default value for the name attribute is an empty string, which is matched like any other name.
+            // https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/client-configuration
             Channel = new ChannelFactory<T>("*");
         }
 
