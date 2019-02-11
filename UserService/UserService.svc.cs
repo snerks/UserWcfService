65f4b54248a814a682c731ace11a17a277bfaa81
snerks <martin.github@outlook.com>
2019-02-11 07:52:12 +0000

    Experiments

--- UserService/UserService.svc.cs
+++ UserService/UserService.svc.cs
@@ -8,10 +8,14 @@ using UserWcfServiceLib;
 
 namespace UserService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu 
+    // to change the class name "UserService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, 
+    // please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
+    //[ServiceContract]
     public class UserService : IUserService
     {
+        //[OperationContract]
         public int? GetUserIdByName(string userName)
         {
             if (userName == @"DOMAIN\j.smith")
--- UserWcfServiceLib/IUserService.cs
+++ UserWcfServiceLib/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define IsWcfService
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
@@ -7,10 +8,14 @@ using System.Threading.Tasks;
 
 namespace UserWcfServiceLib
 {
+#if IsWcfService
     [ServiceContract]
+#endif
     public interface IUserService
     {
+#if IsWcfService
         [OperationContract]
+#endif
         int? GetUserIdByName(string userName);
     }
 }
